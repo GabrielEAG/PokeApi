2cc3e14af19f64bd43c98433cc7e5a5f875f42e2
GabrielEAG <00181423@uca.edu.sv>
2024-12-13 20:59:30 -0600

    a ver si funciona

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config(); // Carga las variables de entorno
+
+const app = express();
+const PORT = 3000;
+
+// Variables de entorno para el login
+const VALID_USER = process.env.USERNAMELOG;
+const VALID_PASS = process.env.PASSWORD;
+
+console.log('Usuario:', process.env.USERNAMELOG);
+console.log('Contraseña:', VALID_PASS);
+
+// Middleware para servir archivos estáticos y parsear JSON
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+// Ruta para manejar el login
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === VALID_USER && password === VALID_PASS) {
+    res.json({ success: true });
+  } else {
+    res.json({ success: false, message: 'Credenciales incorrectas' });
+  }
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
